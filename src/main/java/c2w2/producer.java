07fa28849ba7a87eef780e000e8c520ff3f6e92a
Administrator <Administrator@USER-20130809GE>
2016-08-02 21:01:04 +0900

    3

--- src/main/java/c2w2/producer.java
+++ src/main/java/c2w2/producer.java
@@ -1,23 +1,39 @@
 package c2w2;
 
 import java.util.Properties;
+import java.util.Random;
+
 import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
 
 public class producer{
     public static void main(String[] args) throws Exception {
-        Properties props = new Properties();
+    	Random rand = new Random(System.currentTimeMillis());
+    	Properties props = new Properties();
         props.put("metadata.broker.list", "kafka1:9092,kafkat2:9092,kafka3:9092");
         props.put("serializer.class", "kafka.serializer.StringEncoder");
         ProducerConfig  producerConfig = new ProducerConfig(props);
         Producer<String, String> producer = new Producer<String, String>(producerConfig);
-        
-        for(int i=0; i<10; i++)
+        String str;
+        for(int i=0; i<2147483647; i++)
         {
-        
-        KeyedMessage<String, String> message = new KeyedMessage<String, String>("c2w2", "WOW");  
-        producer.send(message);
+        	if(i%3==0)
+        	{
+        		str=Integer.toString(rand.nextInt(9)+1);
+        		KeyedMessage<String, String> message = new KeyedMessage<String, String>("topic1", str);  
+        		producer.send(message);
+        	}else if(i%3==1)
+        	{
+        		str=Integer.toString(rand.nextInt(9)+1);
+        		KeyedMessage<String, String> message = new KeyedMessage<String, String>("topic2", str);  
+        		producer.send(message);
+        	}else if(i%3==2)
+        	{
+        		str=Integer.toString(rand.nextInt(9)+1);
+        		KeyedMessage<String, String> message = new KeyedMessage<String, String>("topic3", str);  
+        		producer.send(message);
+        	}
         }
         
         producer.close();
